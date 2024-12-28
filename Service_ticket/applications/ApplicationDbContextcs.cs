b1e11d0c76d8fa61f2c143e5480798321c24cdb3
lucasdaconcei <daconceicaolucas681@gmail.com>
2024-12-27 22:40:52 -0300

    Adicionar arquivos de projeto.

---
+++ Service_ticket/applications/ApplicationDbContextcs.cs
@@ -0,0 +1,13 @@
+﻿namespace Service_ticket.applications;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Sockets;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Ticket> Tickets { get; set; }
+}
---
+++ Service_ticket/applications/TabelasTickets.cs
@@ -0,0 +1,10 @@
+﻿namespace Service_ticket.applications;
+
+public class TabelasTickets
+{
+    public int Id { get; set; }
+    public string Assunto { get; set; }
+    public string Mensagem { get; set; }
+    public string Status { get; set; }
+    public DateTime DataCriacao { get; set; }
+}
